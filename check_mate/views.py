ae3ebeb8c39484ff39b25d24ddc94336947cd93f
kellymorin <kmorin06@gmail.com>
2019-03-03 10:42:25 -0600

    updating file structure

--- check_mate/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def index(request):
-    context = {}
-    template_name = 'index.html'
-    return render(request, template_name, context)
