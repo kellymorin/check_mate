0ba6aabba4c4eb8d078dc0903cc360216d858ebe
kellymorin <kmorin06@gmail.com>
2019-03-05 19:27:37 -0600

    Refactor task_edit view function, cleaner and more readable

--- check_mate/views/task_views.py
+++ check_mate/views/task_views.py
@@ -86,30 +86,48 @@ def task_add(request):
 
 @login_required
 def task_edit(request, task_id):
-    # Then they should be presented with a form, that is pre-populated with existing task data, where they can update information about the task such as assigned team member
+    """Handles rendering a pre-populated edit form with existing task data and update of task details
 
-    # Automation Notes:
-    # Currently set up to update ticket if task is updated, but does not currently flow through to projects
+    Arguments:
+        task_id {int} -- The id of the task we would like to edit
 
-    # TODO: Need to add automation to post to task history before saving changes
+    Returns:
+        [render] -- if the request is a GET, will return a render of task_edit.html with pre-populated information in task form
+        [HttpResponseRedirect] -- when the POST request to update a task is successful, it will redirect to the ticket detail view with the task details updated
+    """
 
-    if request.method == "GET":
         task = Task.objects.get(pk=task_id)
-        task_form = TaskForm(instance=task)
-        task_status = TaskStatusForm(instance=task)
         ticket_id = task.ticket.id
         ticket = Ticket.objects.get(pk=ticket_id)
-        template_name = "task_edit.html"
+    project_id = ticket.project.id
+    project = Project.objects.get(pk=project_id)
+    form_data = request.POST
+
+    if request.method == "GET":
+        task_form = TaskForm(instance=task)
+        task_status = TaskStatusForm(instance=task)
         context = {
             "task": task,
             "task_form": task_form,
             "task_status": task_status,
+            "ticket": ticket_id,
             "edit": True
         }
+
         return render(request, "task_form.html", context)
+
     elif request.method == "POST":
+
         if task.task_status != form_data["task_status"]:
             __update_task_history(task, request.user, "Status", form_data["task_status"])
+
+        task.task_name = form_data["task_name"]
+        task.task_status = form_data["task_status"]
+        task.task_description = form_data["task_description"]
+
+        if form_data["task_due"]:
+            task.task_due = form_data["task_due"]
+
         if form_data["task_assigned_user"] != "":
             assigned_user = User.objects.get(pk=form_data["task_assigned_user"])
             if task.task_assigned_user != form_data["task_assigned_user"]:
@@ -117,26 +135,34 @@ def task_edit(request, task_id):
 
             task.task_assigned_user = assigned_user
 
-        if request.POST["task_due"]:
-            task.task_name = request.POST["task_name"]
-            task.task_description = request.POST["task_description"]
-            task.task_status = request.POST["task_status"]
-            task.task_due = request.POST["task_due"]
             task.save()
-        else:
-            task.task_name = request.POST["task_name"]
-            task.task_description = request.POST["task_description"]
-            task.task_status = request.POST["task_status"]
-            task.save()
+
+        if form_data["task_status"] == "Active" and ticket.ticket_status == "Not Started":
+            ticket.ticket_status = "Active"
+            ticket.save()
+
             __update_ticket_history(ticket, request.user, "Status", "Active")
+
+        if ticket.get_task_status['Complete'] == ticket.get_task_status['Total']:
+            ticket.ticket_status = "Complete"
+            ticket.save()
+
             __update_ticket_history(ticket, request.user, "Status", "Complete")
 
             if project.get_ticket_status['Complete'] == project.get_ticket_status['Total']:
                 project.project_status = "Complete"
                 project.save()
+
+        if task.task_status != "Complete" and ticket.ticket_status == "Complete":
             ticket.ticket_status = "Active"
             ticket.save()
+
             __update_ticket_history(ticket, request.user, "Status", "Active")
+
+            if project.project_status == "Complete":
+                project.project_status = "Active"
+                project.save()
+
         return HttpResponseRedirect(reverse("check_mate:ticket_details", args=(ticket_id,)))
 
 
